be04e555a3b01bc6ac1b8da3d88140197d116771
Jack Henley <jack.d.henley@gmail.com>
2014-03-11 17:51:31 -0400

    Fixed merge errors, working

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-// TODO: Add angular application module and routes.
-=======
->>>>>>> 0066b905c63ce1a88ba1da566db6da7e46a1ecb5
+
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 //= require_self
@@ -12,28 +9,24 @@
 
 var StoreFront = angular.module('StoreFront',['ngRoute']);
 
-<<<<<<< HEAD
-StoreFront.config(['$routeProvider', function($routeProvider){
-    // Default Route
-    $routeProvider.otherwise({
-        templateUrl: '../assets/mainIndex.html',
-        controller: 'IndexCtrl'
-    });
+// <<<<<<< HEAD
+// StoreFront.config(['$routeProvider', function($routeProvider){
+//     // Default Route
+//     $routeProvider.otherwise({
+//         templateUrl: '../assets/mainIndex.html',
+//         controller: 'IndexCtrl'
+//     });
 
-=======
+// =======
 // Angular routes
 StoreFront.config(['$routeProvider', function($routeProvider){
->>>>>>> 0066b905c63ce1a88ba1da566db6da7e46a1ecb5
     // Route to retrieve one product
     // '/product/:productId
     $routeProvider.when('/product/:productId',{
         templateUrl: '../assets/mainProduct.html',
         controller: 'ProductCtrl'
     });
-<<<<<<< HEAD
-}]);
 
-=======
 
     // Default Route
     $routeProvider.otherwise({
@@ -41,4 +34,3 @@ StoreFront.config(['$routeProvider', function($routeProvider){
         controller: 'IndexCtrl'
     });
 }]);
->>>>>>> 0066b905c63ce1a88ba1da566db6da7e46a1ecb5
